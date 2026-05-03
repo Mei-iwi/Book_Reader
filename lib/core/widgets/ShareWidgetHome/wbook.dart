9d05212d1fdd0fad868428ca8bf6cf7bfd799aa7
Mei-IwI <c2005vn@gmail.com>
2026-05-03 21:02:32 +0700

    pull origin main

--- lib/core/widgets/ShareWidgetHome/wbook.dart
+++ lib/core/widgets/ShareWidgetHome/wbook.dart
@@ -69,111 +69,116 @@ void showBookSnackBar(
         ),
 
         duration: const Duration(seconds: 5),
-        content: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Container(
-              width: 90,
-              height: 100,
-              decoration: BoxDecoration(
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.15),
-                    offset: Offset(2, 4),
-                    blurRadius: 12,
+        content: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                width: 90,
+                height: 100,
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.15),
+                      offset: Offset(2, 4),
+                      blurRadius: 12,
+                    ),
+                  ],
+                  borderRadius: BorderRadius.circular(15),
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: (checkSourceImage(urlImage: url)
+                        ? AssetImage(url)
+                        : NetworkImage(url)),
                   ),
-                ],
-                borderRadius: BorderRadius.circular(15),
-                image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: (checkSourceImage(urlImage: url)
-                      ? AssetImage(url)
-                      : NetworkImage(url)),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 15),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    title.length > 10 ? "${title.substring(0, 10)}..." : title,
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.grey,
+              Padding(
+                padding: const EdgeInsets.only(left: 15),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      title.length > 10
+                          ? "${title.substring(0, 10)}..."
+                          : title,
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey,
+                      ),
                     ),
-                  ),
-                  Text(
-                    author.length > 15
-                        ? "${author.substring(0, 15)}..."
-                        : author,
-                    style: TextStyle(fontSize: 15, color: Colors.grey),
-                  ),
-                ],
+                    Text(
+                      author.length > 15
+                          ? "${author.substring(0, 15)}..."
+                          : author,
+                      style: TextStyle(fontSize: 15, color: Colors.grey),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 15),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => Details(
-                            title: title,
-                            url: url,
-                            value: 1,
-                            total: 3636,
-                            assetPath:
-                                'assets/sample_data/templatecontentbooks/hoang_tu_be_demo.txt',
-                            author: author,
-                            views: 0,
+              Padding(
+                padding: const EdgeInsets.only(right: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => Details(
+                              title: title,
+                              url: url,
+                              value: 1,
+                              total: 3636,
+                              assetPath:
+                                  'assets/sample_data/templatecontentbooks/hoang_tu_be_demo.txt',
+                              author: author,
+                              views: 0,
+                            ),
                           ),
+                        );
+                      },
+
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: Size(120, 40),
+
+                        backgroundColor: Colors.blue,
+                      ),
+                      child: Text(
+                        'Detail',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
                         ),
-                      );
-                    },
-
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: Size(120, 40),
-
-                      backgroundColor: Colors.blue,
-                    ),
-                    child: Text(
-                      'Detials',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
                       ),
                     ),
-                  ),
-                  SizedBox(height: 10),
-                  ElevatedButton(
-                    onPressed: onDownload,
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: Size(120, 40),
-                      backgroundColor: Colors.white,
-                      shape: RoundedRectangleBorder(
-                        side: BorderSide(width: 2, color: Colors.blue),
-                        borderRadius: BorderRadius.circular(100),
+                    SizedBox(height: 10),
+                    ElevatedButton(
+                      onPressed: onDownload,
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: Size(120, 40),
+                        backgroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          side: BorderSide(width: 2, color: Colors.blue),
+                          borderRadius: BorderRadius.circular(100),
+                        ),
+                      ),
+                      child: Text(
+                        'Download',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue,
+                        ),
                       ),
                     ),
-                    child: Text(
-                      'Download',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.blue,
-                      ),
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
