84b87526f48ff93d9d97a829d9650018f965224d
Mei-IwI <c2005vn@gmail.com>
2026-05-30 16:04:42 +0700

    connect database entity

---
+++ backend/BookReader.Api/Entities/ReadingProgress.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookReader.Api.Entities;
+
+public partial class ReadingProgress
+{
+    public int Id { get; set; }
+
+    public int UserId { get; set; }
+
+    public int BookId { get; set; }
+
+    public int CurrentPage { get; set; }
+
+    public decimal ProgressPercent { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+    public virtual Book Book { get; set; } = null!;
+
+    public virtual AppUser User { get; set; } = null!;
+}
